32508a2e54c1652b436dade1cc110d0b41dbebd5
zhangzhandong <747488757@qq.com>
2024-11-23 22:50:47 +0800

    test

--- src/main/java/org/sang/HelloController.java
+++ src/main/java/org/sang/HelloController.java
@@ -4,14 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class TestController {
-    @GetMapping("/test")
+public class HelloController {
+    @GetMapping("/hello")
     public String hello() {
         return "hello spring boot!";
-    }
-
-    @GetMapping("/test2")
-    public String hello2() {
-        return "hello test 2!";
     }
 }
